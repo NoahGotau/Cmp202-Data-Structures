0c4c7adaa4ab616f5a7e656481590811d2044289
NoahGotau <129745182+NoahGotau@users.noreply.github.com>
2024-06-29 11:27:45 +0100

    Add files via upload

---
+++ java-data-structures/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Stack myStack = new Stack(2);
+        myStack.push(7);
+        myStack.push(5);
+        myStack.pop();
+        myStack.printAll();
+    }
+}
